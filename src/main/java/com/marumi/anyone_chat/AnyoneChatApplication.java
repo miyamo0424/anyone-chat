9f9e5a138108f36895f533fe2c674909b44728b8
TEMI0424\Poket\Git <te.mi0424@gmail.com>
2025-06-12 23:50:38 +0900

    spring initializrのテンプレートをコミット

---
+++ src/main/java/com/marumi/anyone_chat/AnyoneChatApplication.java
@@ -0,0 +1,13 @@
+package com.marumi.anyone_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyoneChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnyoneChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marumi/anyone_chat/AnyoneChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marumi.anyone_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyoneChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
